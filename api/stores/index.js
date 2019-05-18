8e07e307844514d713d79735e53c1d5f9311729d
Diogo Vianna V. Araujo <diogo@velabikes.com.br>
2019-05-18 18:27:18 +0000

    Added endpoint to fetch store information from google

--- api/stores/index.js
+++ api/stores/index.js
@@ -1,13 +1,27 @@
-const googleMapsClient = require('@google/maps')
+const GoogleMaps = require('@google/maps')
 
-module.exports = (req, res) => {
-  const googleMapsClient.createClient({
+module.exports = async (req, res) => {
+  const googleMapsClient = GoogleMaps.createClient({
     key: process.env.GOOGLE_MAPS_SERVER_KEY
   })
 
-  googleMapsClient.findPlace({ input: 'Vela Bikes Sao Paulo', inputtype: 'textquery' }, (err, response) => {
-    console.log(response)
-    if (!err) return res.end(JSON.stringify(response))
-    return res.end(JSON.stringify(err))
-  })
+  const placeIdArray = [
+    'ChIJq6obEqBXzpQROBi2WivMFh8',
+    'ChIJzZtKMWg7WpMRrWtOjWWtH2M',
+    'ChIJb00g-rHVmwARzTLLKX3i-lU',
+    'ChIJC94Ll0vl3JQRwtMIVjOnAf8'
+  ]
+
+  const fetchPlace = placeid =>
+    new Promise((resolve, reject) =>
+      googleMapsClient.place({ placeid }, (err, result) => {
+        if (!err) return resolve(result)
+      })
+    )
+
+  const placeArray = await Promise.all(placeIdArray.map(fetchPlace))
+
+  console.log(placeArray)
+
+  return res.end(JSON.stringify(placeArray))
 }
