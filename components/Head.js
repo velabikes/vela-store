aaf96a274601be64b3a261e80b215d372c5e339e
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2019-06-10 09:42:11 -0300

    Added google tag manager

--- components/Head.js
+++ components/Head.js
@@ -12,6 +12,13 @@ const CustomHead = props =>
       <link rel='icon' type='image/png' sizes='16x16' href='/static/fav/favicon-16x16.png' />
       <link rel='icon' type='image/x-icon' href='/static/fav/favicon.ico' />
       <meta name='theme-color' content='#ffffff' />
+      <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-K9555FS');
+      </script>
     </Head>
     <style jsx global>{`
       html, body {
