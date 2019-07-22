9c1f175b29e29fa9d8f757c3e495b22d8cf3cbab
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2019-07-22 12:21:10 -0300

    back button now a feature of card component.

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,22 @@
-const Card = ({children}) =>
+import { BackIcon } from 'components/Icons'
+
+const Card = ({children, onBackClick}) =>
   <div className='Card'>
     {children}
+    {onBackClick && <a onClick={onBackClick}>
+      <BackIcon />
+    </a>}
     <style jsx>{`
       .Card {
         padding: 2rem;
         box-shadow: 0px 18px 9px -18px rgba(100,100,100, 0.3);
+        position: relative;
+      }
+      a {
+        position: absolute;
+        right: 0;
+        top: 0;
+        padding: 1rem;
       }
     `}</style>
   </div>
