1f26913c8db7f0e37f1314288b571560220c19a8
Diogo Vianna <diogovaraujo@gmail.com>
2019-07-25 13:39:50 -0300

    Fixed wrong background on home vela network cta

--- components/PaddedView.js
+++ components/PaddedView.js
@@ -2,18 +2,20 @@ import PropTypes from 'prop-types'
 
 const PaddedView = props =>
   <div className='PaddedView' {...props}>
-    {props.children}
+    <div className='PaddedViewWrapper'>
+      {props.children}
+    </div>
     <style jsx>{`
-      div {
+      .PaddedViewWrapper {
         padding: 0 1rem;
       }
       @media only screen and (min-width: 400px) {
-        div {
+        .PaddedViewWrapper {
           padding: 0rem 2rem;
         }
       }
       @media only screen and (min-width: 1366px) {
-        div {
+        .PaddedViewWrapper {
           width: 100%;
           max-width: 1300px;
           margin: 0 auto;
