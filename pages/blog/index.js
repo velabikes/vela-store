9ea37f7567ce2ab50a69aa1fb957a0fa73da73e6
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2019-08-14 16:11:30 -0300

    Added base blog structure

---
+++ pages/blog/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import PaddedView from 'components/PaddedView'
+import Main from 'components/Main'
+import MainHeader from 'components/MainHeader'
+
+const Blog = () =>
+  <PaddedView>
+    <Main>
+      <Head>
+        <title>Blog - Vela Bikes</title>
+      </Head>
+      <MainHeader
+        title={Blog}
+      />
+    </Main>
+  </PaddedView>
+
+export default Blog
