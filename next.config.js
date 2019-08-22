b2f14a57612adc3a4a30463013d1d3287d356a6e
Diogo Vianna <diogo@velabikes.com.br>
2019-08-22 01:43:32 +0000

    Added webpack visualiser

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const nextConfig = {
   target: 'serverless',
 }
+
+module.exports = withBundleAnalyzer(nextConfig)
