f0a09a8bbd1563924b34fbe204cbf0183a005340
Amanda Albuquerque <amandlei@hotmail.com>
2019-09-14 15:13:12 -0300

    Fix Head component lint errors

--- components/head/FacebookPixel.js
+++ components/head/FacebookPixel.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import Head from 'next/head'
 
-const FacebookPixel = () => 
+const FacebookPixel = () => (
   <Head>
-    <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+    <script dangerouslySetInnerHTML={{
+      __html: `!function(f,b,e,v,n,t,s)
       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
       if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -14,9 +15,11 @@ const FacebookPixel = () =>
       fbq('init', '875099512610020');
       fbq('track', 'PageView');` }}
     />
-    <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none"
+    <noscript dangerouslySetInnerHTML={{
+      __html: `<img height="1" width="1" style="display:none"
       src="https://www.facebook.com/tr?id=875099512610020&ev=PageView&noscript=1" />` }}
     />
   </Head>
+)
 
 export default FacebookPixel
