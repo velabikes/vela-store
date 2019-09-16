7eef55bf8c71c21292ce89545884db7314d70ae9
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2019-09-16 14:48:18 -0300

    Blog page now uses post_date field instead of publication date.

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -7,7 +7,7 @@ import Grid from 'components/Grid'
 import { Date, RichText } from 'prismic-reactjs'
 import dayjs from 'dayjs'
 
-const BlogPostPage = ({ cover, title, teaser, body, date }) =>
+const BlogPostPage = ({ cover, title, teaser, body, post_date }) =>
   <PaddedView>
     <Main>
       <Head>
@@ -17,7 +17,7 @@ const BlogPostPage = ({ cover, title, teaser, body, date }) =>
         title={title[0].text}
         tagline={<div>
           <div>{teaser[0].text}</div>
-          <div><small>{dayjs(Date(date)).format('DD/MM/YYYY - HH:MM')}</small></div>
+          <div><small>{dayjs(Date(post_date)).format('DD/MM/YYYY')}</small></div>
         </div>}
       />
       <Grid template='1fr 1fr'>
@@ -39,9 +39,9 @@ const BlogPostPage = ({ cover, title, teaser, body, date }) =>
 BlogPostPage.getInitialProps = async ({ req, query }) => {
   const { slug } = query
   const prismic = await getPrismic(req)
-  const { data, last_publication_date } = await prismic.getByUID('blog_post', slug)
+  const { data } = await prismic.getByUID('blog_post', slug)
 
-  return { ...data, date: last_publication_date }
+  return { ...data }
 }
 
 export default BlogPostPage
