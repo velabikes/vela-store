010a2e6ed6232bfb0647971dbaa805e44883f6a8
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2019-09-20 11:37:35 -0300

    Component retuse on section

--- components/home/Highlight.js
+++ components/home/Highlight.js
@@ -1,43 +1,44 @@
 import Grid from 'components/Grid'
 import Button from 'components/Button'
 import Price from 'components/Price'
-import PaddedView from '../PaddedView'
+import PaddedView from 'components/PaddedView'
+import Section from 'components/Section'
+import SectionHeader from 'components/SectionHeader'
 
 const Highlight = () => {
   return (
     <PaddedView style={{ background: '#edeef2' }}>
-      <section>
-        <Grid template='1fr 1fr'>
-          <div className='content'>
-            <Grid template='1fr 1fr'>
-              <h1>Vela 1</h1>
-              <div><Button>Saiba +</Button></div>
-            </Grid>
-            <p style={{fontSize: '1.25em'}}>A combinação perfeita entre design clássico e tecnologia de ponta.</p>
-            <p>A Vela é uma bicicleta elétrica completa para o uso diário urbano, com funcionamento simples, baixa manutenção e muito conforto.</p>
-            <p><strong><Price value={5890} /></strong></p>
-            <br />
-            <Grid template='1fr 1fr'>
-              <div className='detail'>
-                <strong>até 40km</strong>
-                <div>Potencia do motor.</div>
-              </div>
-              <div className='detail'>
-                <strong>350W</strong>
-                <div>Autonomia da bateria.</div>
-              </div>
-            </Grid>
-          </div>
-          <div>
-            <img src='/static/home/vela-1-frente.png' alt='Vela 1 Quadro Baixo de Frente' />
-          </div>
-        </Grid>
-      </section>
+      <Grid template='2fr 3fr'>
+        <Section>
+          <Grid template='1fr 1fr'>
+            <SectionHeader title='Vela 1' />
+            <div><Button primary outline>Saiba +</Button></div>
+          </Grid>
+          <p className='teaser'>A combinação perfeita entre design clássico e tecnologia de ponta.</p>
+          <p>A Vela é uma bicicleta elétrica completa para o uso diário urbano, com funcionamento simples, baixa manutenção e muito conforto.</p>
+          <br />
+          <p><strong><Price value={5890} /></strong></p>
+          <br />
+          <Grid template='1fr 1fr'>
+            <div className='detail'>
+              <strong>até 40km</strong>
+              <div>Potencia do motor.</div>
+            </div>
+            <div className='detail'>
+              <strong>350W</strong>
+              <div>Autonomia da bateria.</div>
+            </div>
+          </Grid>
+        </Section>
+        <div className='image'>
+          <img src='/static/home/vela-1-frente.png' alt='Vela 1 Quadro Baixo de Frente' />
+        </div>
+      </Grid>
       <style jsx>{`
-        .content {
-          padding: 3rem 0;
+        .teaser {
+          font-size: 1.5em;
         }
-        .content > p > strong {
+        strong {
           font-size: 1.5em;
         }
         .detail {
@@ -45,6 +46,10 @@ const Highlight = () => {
         }
         .detail strong {
           font-size: 1.5em;
+          font-style: italic;
+        }
+        .image {
+          align-self: end;
         }
       `}</style>
     </PaddedView>
