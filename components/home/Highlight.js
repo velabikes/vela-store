34dc50c37647deda7b9c546b515e3d9102a27066
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2019-09-20 10:05:06 -0300

    Correct text on home highlight.

--- components/home/Highlight.js
+++ components/home/Highlight.js
@@ -11,8 +11,8 @@ const Highlight = () => {
             <h1>Vela 1</h1>
             <Button>Saiba +</Button>
           </Grid>
-          <div style={{fontSize: '1.25em'}}>asdfasdf</div>
-          <div>asdfasdf</div>
+          <p style={{fontSize: '1.25em'}}>A combinação perfeita entre design clássico e tecnologia de ponta.</p>
+          <p>A Vela é uma bicicleta elétrica completa para o uso diário urbano, com funcionamento simples, baixa manutenção e muito conforto.</p>
         </div>
         <div>
           <img src='/static/home/vela-1-frente.png' />
