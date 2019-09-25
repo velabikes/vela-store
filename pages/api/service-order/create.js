a4b4e983b69ec0e65d1f8df7b2c9e176e8337c66
Guilherme Oliveira <guilherme@velabikes.com.br>
2019-09-25 11:29:21 -0300

    add pipefycreate on omiejs

--- pages/api/service-order/create.js
+++ pages/api/service-order/create.js
@@ -1,4 +1,5 @@
 import { createCustomer, createServiceOrder } from 'lib/omie'
+import pipefy from 'lib/pipefy'
 
 const handle = async (req, res) => {
   const customer = await createCustomer({
@@ -7,11 +8,28 @@ const handle = async (req, res) => {
   })
 
   const serviceOrder = await createServiceOrder({
-    customerId: customer.extId,
+    customerId: customer.id,
     extId: 17149242
   })
 
-  console.log(serviceOrder)
+  const card = await pipefy.createCard({
+    input: {
+      pipe_id: '1053749',
+      title: 'name',
+      fields_attributes: [
+        { field_id: 'nome', field_value: 'name' },
+        { field_id: 'cpf', field_value: 'document' },
+        { field_id: 'telefone', field_value: 'phone' },
+        { field_id: 'email', field_value: 'email' },
+        { field_id: 'cep', field_value: 'zip' },
+        { field_id: 'numero', field_value: 'number' },
+        { field_id: 'complemento', field_value: 'complement' },
+        { field_id: 'servico', field_value: 'service' }
+      ]
+    }
+  })
+
+  console.log(card)
 }
 
 export default handle
