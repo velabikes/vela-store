7ecb0a0a4cbd7da793bd578c6e997336c682a513
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2019-09-25 17:07:59 -0300

    Added static service order form.

--- pages/api/service-order/create.js
+++ pages/api/service-order/create.js
@@ -1,23 +1,33 @@
-import { createCustomer, createServiceOrder } from 'lib/omie'
+import { putCustomer, createServiceOrder, findCustomer } from 'lib/omie'
 import { createCard } from 'lib/pipefy'
 
 const handle = async (req, res) => {
-  const customer = await createCustomer({
-    extId: 123,
-    name: 'teste da silva'
+  const { name, doc, description } = req.body
+
+  if (!name || !doc || !description) return res.end('Missing fields.')
+
+  const existingCustomer = await findCustomer({ doc })
+
+  const customer = await putCustomer({
+    ...existingCustomer && { id: existingCustomer.id },
+    ...!existingCustomer && { extId: new Date().valueOf() },
+    name
   })
 
   const serviceOrder = await createServiceOrder({
     customerId: customer.id,
-    extId: 17149242
+    extId: new Date().valueOf(),
+    description
   })
+
+  console.log(serviceOrder)
 
   const card = await createCard({
     input: {
       pipe_id: '1053749',
-      title: 'name',
+      title: `asdfasdfasfsd`,
       fields_attributes: [
-        { field_id: 'nome', field_value: 'name' },
+        { field_id: 'nome', field_value: name },
         { field_id: 'cpf', field_value: 'document' },
         { field_id: 'telefone', field_value: 'phone' },
         { field_id: 'email', field_value: 'email' },
@@ -30,6 +40,8 @@ const handle = async (req, res) => {
   })
 
   console.log(card)
+
+  res.end('ok')
 }
 
 export default handle
