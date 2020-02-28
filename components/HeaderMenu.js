b35f8a6478afd33ae77da5808b8be7aed08e332c
Dagel <giovanna@velabikes.com.br>
2020-02-28 12:07:45 -0300

    Update HeaderMenu.js for top web menu without icon

--- components/HeaderMenu.js
+++ components/HeaderMenu.js
@@ -1,25 +1,28 @@
-import Link from 'next/link'
 import PropTypes from 'prop-types'
 import { compose, withHandlers } from 'recompose'
 import { toggleDrawer } from '../lib/redux'
 import withOpenDrawer from '../containers/withOpenDrawer'
-import { Logo, MenuIcon, CartIcon } from './Icons'
+import { MenuIcon, CartIcon } from './Icons'
 
 /* eslint-disable */
 const HeaderMenu = ({ handleCartClick, handleMenuClick }) =>
   <>
-    <div><Link href='/'><a>
-      <Logo />
-    </a></Link></div>
-    <div><a onClick={handleMenuClick}>
+    <div className='menu'><a onClick={handleMenuClick}>
       <MenuIcon />
     </a></div>
     <div><a onClick={handleCartClick}>
       <CartIcon />
     </a></div>
+    <style jsx>{`
+      @media only screen and (min-width: 768px) {
+        .menu {
+          display: none;
+        }
+    `}
+    </style>
   </>
-/* eslint-enable */
 
+/* eslint-enable */
 HeaderMenu.propTypes = {
   handleCartClick: PropTypes.func,
   handleMenuClick: PropTypes.func
