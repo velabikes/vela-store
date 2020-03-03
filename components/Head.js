1a891093a61efb91af27f13838ba705cb05560a2
Dagel <giovanna@velabikes.com.br>
2020-03-02 22:54:27 -0300

    font fixes for v2 landing

--- components/Head.js
+++ components/Head.js
@@ -11,7 +11,7 @@ const CustomHead = props =>
       <script dangerouslySetInnerHTML={{ __html: "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K9555FS');" }} />
       <meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' />
       <meta charSet='utf-8' />
-      <link href='https://fonts.googleapis.com/css?family=Barlow:400,500,600,700&display=swap' rel='stylesheet' />
+      <link href='https://fonts.googleapis.com/css?family=Barlow:400,500,600,700,900&display=swap' rel='stylesheet' />
       <link rel='icon' type='image/png' sizes='32x32' href='/static/fav/favicon-32x32.png' />
       <link rel='icon' type='image/png' sizes='96x96' href='/static/fav/favicon-96x96.png' />
       <link rel='icon' type='image/png' sizes='16x16' href='/static/fav/favicon-16x16.png' />
