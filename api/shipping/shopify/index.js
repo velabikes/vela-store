dc5b6fe9e87b2bff35496e7f269bc81c56b3862b
Diogo <diogo@velabikes.com.br>
2020-03-20 02:51:22 -0300

    Fixed wrong price on shipping.

--- api/shipping/shopify/index.js
+++ api/shipping/shopify/index.js
@@ -57,7 +57,7 @@ module.exports = async (req, res) => {
         rates: [{
           service_name: 'Transportadora',
           service_code: 'FGN',
-          total_price: '89',
+          total_price: '8900',
           description: 'Produção + 18 dias úteis',
           currency: 'BRL'
         }]
