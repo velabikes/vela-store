5c35f6833996c2389ad845e102b1f5691e775932
Dagel <giovanna@velabikes.com.br>
2020-03-28 15:25:37 -0300

    Control styling

--- components/form/Control.js
+++ components/form/Control.js
@@ -1,19 +1,24 @@
+import { velaBlue, midGray } from 'style/colors'
+import PropTypes from 'prop-types'
 
 const Control = ({ field, options }) => {
   const { value, onChange, name } = field
   const setValue = onChange(name)
   const renderOption = option =>
-    <div onClick={() => setValue(option)}>
+    <div onClick={() => setValue(option)} onKeyDown={() => setValue(option)} role='button' tabIndex={0}>
       {option}
       <style jsx>{`
         div {
           flex: 1;
+          padding: 0.8em;
+          border-radius: 2em;
+          font-weight: 600;
           text-align: center;
+          transition: 1s;
           ${value === option && `
-            background-color: red;
-            color: white;
-          `}
-        }
+          color: white;
+          background-color: ${velaBlue}
+        `}
       `}</style>
     </div>
 
@@ -22,12 +27,20 @@ const Control = ({ field, options }) => {
       {options.map(renderOption)}
       <style jsx>{`
         div {
-          border: 1px solid red;
+          border-radius: 2em;
+          border: 1px solid ${midGray};
           display: flex;
+          color: ${midGray};
         }
+
       `}</style>
     </div>
   )
+}
+
+Control.propTypes = {
+  field: PropTypes.any,
+  options: PropTypes.any
 }
 
 export default Control
