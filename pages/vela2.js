f9358b61efa9525ca26f95bfe1bed7d2429c9bd5
Dagel <giovanna@velabikes.com.br>
2020-03-28 15:27:46 -0300

    Update Configure.js in vela2.js

--- pages/vela2.js
+++ pages/vela2.js
@@ -1,14 +1,15 @@
 import Head from 'next/head'
 import Display from '../components/vela2/Display'
 import Bar from '../components/vela2/Bar'
-import ModelSelector from '../components/vela2/ModelSelector'
+import Configure from '../components/vela2/Configure'
+
 const Vela2 = () =>
   <div className='vela2'>
     <Head>
       <title>Vela 2 - Vela Bikes</title>
     </Head>
     <Display />
-    <ModelSelector />
+    <Configure />
     <Bar />
   </div>
 export default Vela2
