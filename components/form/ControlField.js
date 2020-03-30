fdbac9b437dd1638586e40775e92f41db9d12359
Dagel <giovanna@velabikes.com.br>
2020-03-30 18:07:37 -0300

    ControlField.js remove label

--- components/form/ControlField.js
+++ components/form/ControlField.js
@@ -2,19 +2,12 @@ import Field from 'components/form/Field'
 import Control from 'components/form/Control'
 import PropTypes from 'prop-types'
 
-const ControlField = ({ label, selected, ...props }) =>
+const ControlField = (props) =>
   <div>
-    <label>{label}</label><span>{selected}</span>
     <Field
       component={Control}
       {...props}
     />
-    <style jsx>{`
-      label {
-        display: inline-block;
-        margin: 0.5em;
-      }
-    `}</style>
   </div>
 
 ControlField.propTypes = {
