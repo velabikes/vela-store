3e07227d30989ba5cc64cbb7d99e8fc13af4592d
Diogo <diogo@velabikes.com.br>
2020-04-02 10:58:40 -0300

    Add to cart.

--- pages/vela2.js
+++ pages/vela2.js
@@ -1,16 +1,39 @@
 import { useState } from 'react'
 import Head from 'next/head'
+import { compose } from 'recompose'
 import Display from '../components/vela2/Display'
 import Bar from '../components/vela2/Bar'
 import ModelSelector from '../components/vela2/ModelSelector'
 import ExtraSelector from '../components/vela2/ExtraSelector'
 import Tab from '../components/vela2/Tab'
 import { lightGray } from '../style/colors'
+import withCheckoutLineItemsAdd from '../containers/withCheckoutLineItemsAdd'
+import withCheckoutId from '../containers/withCheckoutId'
+import withCheckout from '../containers/withCheckout'
 
-const Vela2 = () => {
+const Vela2 = ({ checkout, checkoutId, checkoutLineItemsAdd }) => {
   const [selectedModel, setSelectedModel] = useState({})
   const [selectedExtra, setSelectedExtra] = useState([])
   const [step, setStep] = useState(1)
+
+  const handleNext = async () => {
+    if (step === 2) {
+      await checkoutLineItemsAdd({
+        variables: {
+          checkoutId,
+          lineItems: selectedExtra.map(({ id }) =>
+            ({
+              variantId: id,
+              quantity: 1
+            })
+          )
+        }
+      })
+      window.location.replace(checkout.webUrl)
+    }
+
+    setStep(2)
+  }
 
   return (
     <div className='vela2'>
@@ -31,9 +54,13 @@ const Vela2 = () => {
           }
         />
       </Tab>
-      <Bar model={selectedModel} extra={selectedExtra} onContinue={() => setStep(2)} />
+      <Bar model={selectedModel} extra={selectedExtra} onContinue={handleNext} />
     </div>
   )
 }
 
-export default Vela2
+export default compose(
+  withCheckout,
+  withCheckoutLineItemsAdd,
+  withCheckoutId
+)(Vela2)
