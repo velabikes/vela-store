5366ec9821a6af485c733893e2722a3076dd9cd4
Mastereloading <guilherme-lamon@hotmail.com>
2021-01-05 13:01:19 -0300

    Fixing shipping price 2021

--- api/shipping/shopify/index.js
+++ api/shipping/shopify/index.js
@@ -68,8 +68,7 @@ module.exports = async (req, res) => {
         service_name: 'Frete Grátis',
         service_code: 'FG',
         total_price: '0',
-        // description: '6-11 dias úteis', RECESSO
-        description: '21-26 dias úteis',
+        description: '6-11 dias úteis',
         currency: 'BRL'
       }]
     }))
@@ -133,8 +132,7 @@ const mapCorreiosResultToRate = (result) => result.map(r => {
     service_name: `Sedex`,
     service_code: r.Codigo,
     total_price: parseFloat((r.Valor.split(',').join('.')) * 100),
-    // description: `${r.PrazoEntrega} dias úteis`, RECESSO
-    description: `${Number(r.PrazoEntrega) + 15} dias úteis`,
+    description: `${r.PrazoEntrega} dias úteis`,
     currency: `BRL`
   })
 
