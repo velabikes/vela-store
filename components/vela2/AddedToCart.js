7e1ac51caeee79775270d4b2013ed3a25787f5fc
gidagel <gidagel@gmail.com>
2021-03-13 19:42:22 -0300

    Built V2 improvemens

--- components/vela2/AddedToCart.js
+++ components/vela2/AddedToCart.js
@@ -7,16 +7,17 @@ const AddedToCart = ({ checkout }) => {
   return (
     <div>
       <h3>Itens adicionados ao carrinho!</h3>
-      <h4>Reserve sua Vela 2 e pague o restante quando recebê-la.</h4>
+      <h5>Reserve sua Vela 2 e pague o restante apenas quando ela ficar pronta.</h5>
       <div className='conditions'>
         <p>1. Pague agora R$ 100,00 adiantados (mais quaisquer acessórios e/ou custos de frete).</p>
-        <p>2. Cobraremos os <b>R$ 7.490,00</b> restantes apenas quando sua bike for entregue.</p>
-        <p>3. Sem taxas adicionais - <i>o valor total é o mesmo.</i></p>
+        <p>2. Cobraremos os <b>R$ 7.490,00</b> restantes apenas quando sua bike estiver para ser entregue.</p>
+        <p>3. Sem taxas adicionais surpresas - <i>o valor total é o mesmo.</i></p>
         <p>4. A reserva é reembolsável a qualquer momento antes da entrega.</p>
+        <p>5. Possui alguma dúvida? Envie uma mensagem no chat em azul à direita da tela!</p>
       </div>
-      <p>Aceitamos pagamentos via PayPal em até 12x sem juros no cartão de crédito.</p>
+      <p>Aceitamos pagamentos pelo <b>PayPal</b> (parcele em até 12x sem juros no cartão de crédito) ou pelo Coinbase com criptomoedas.</p>
       {checkout && !checkout.lineItems.edges.length ? 
-      <p>Seu carrinho está vazio.</p> : 
+      <h5>Seu carrinho está vazio.</h5> : 
       <h5>Confira e revise os itens do pedido para finalizar:</h5>
       }
       <div className='items'>
--- components/vela2/Display.js
+++ components/vela2/Display.js
@@ -1,12 +1,12 @@
 import { offWhite, darkGray } from '../../style/colors.js'
 import ModelData from './ModelData.js'
+import {HideOnMobile} from '../HideOn.js'
 
 const Display = ({ model: { frame, size, color, tire } }) => {
   const selectedModelData = ModelData[JSON.stringify({ frame, size, color, tire })] || {}
 
   return (
     <div className='Display'>
-      <h1>Reserva Vela 2</h1>
       <img
         src={(selectedModelData.photos && selectedModelData.photos[0])}
         alt='Vela 2'
@@ -14,12 +14,14 @@ const Display = ({ model: { frame, size, color, tire } }) => {
       <style jsx>{`
       .Display {
         background-color: ${offWhite};
-        padding: 1.5em 1em 0 1em;
+        padding: 1em 0 0 0;
         display: flex;
         align-items: center;
         justify-content: center;
         flex-direction: column;
         flex: 1;
+        position: fixed;
+        z-index: 100;
       }
       h1 {
         color: ${darkGray};
@@ -27,17 +29,24 @@ const Display = ({ model: { frame, size, color, tire } }) => {
         text-align: center;
       }
       .Display img {
-        width: 100%
+        object-fit: cover;
+        max-height: 25vh;
+        object-position: 50% -20%;
       }
 
       @media only screen and (min-width: 768px) {
+        .Display {
+          position: relative;
+        }
         h1 {
           font-size: 5rem;
         }
         .Display img {
           position: relative;
-          width: auto;
-          max-height: 60vh;
+          max-width: 70vw;
+          object-fit: cover;
+          max-height: 100%;
+          object-position: 50% 50%;
         }
       }
     `}</style>
