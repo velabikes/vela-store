e089e6d295c525fb78768e2c0c7239f5a9f1d131
gidagel <gidagel@gmail.com>
2021-06-01 16:49:42 +0200

    Update some SEO basics

--- pages/rental.js
+++ pages/rental.js
@@ -12,7 +12,7 @@ const Rental = () => {
   return (
     <div className="rental">
       <Head>
-        <title>Pacote Aluguel - Vela Bikes</title>
+        <title>Bain + Vela: Alugue a Vela 2 - Vela Bikes</title>
         <link
           rel="alternate"
           hrefLang="pt-br"
@@ -26,18 +26,18 @@ const Rental = () => {
         />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@velabikes" />
-        <meta name="twitter:title" content="Pacote Aluguel - Vela Bikes" />
+        <meta name="twitter:title" content="Bain + Vela: Alugue a Vela 2 - Vela Bikes" />
         <meta
           name="twitter:description"
           content="Pacotes de aluguel para você experimentar a Vela 2! A primeira smartbike do Brasil com condições exclusivas para chamar de sua."
         />
         <meta
           name="twitter:image"
-          content="https://firebasestorage.googleapis.com/v0/b/vela-c1f68.appspot.com/o/public%2Fvelastore%2Frent%2FAluguel-Bike-Aplicativo.jpg?alt=media&token=a5714d99-03c9-4df7-bd9e-8e89c3c83f04"
+          content="https://firebasestorage.googleapis.com/v0/b/vela-c1f68.appspot.com/o/public%2Fvelastore%2Frent%2FAlugue-Vela-2-Bain.jpg?alt=media&token=0f92939b-a51b-4d01-afc1-5acff3b41605"
         />
         <meta property="og:url" content="https://velabikes.com.br" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Pacote Aluguel - Vela Bikes" />
+        <meta property="og:title" content="Bain + Vela: Alugue a Vela 2 - Vela Bikes" />
         <meta property="og:locale" content="pt_BR" />
         <meta
           property="og:description"
@@ -45,7 +45,7 @@ const Rental = () => {
         />
         <meta
           property="og:image"
-          content="https://firebasestorage.googleapis.com/v0/b/vela-c1f68.appspot.com/o/public%2Fvelastore%2Frent%2FAluguel-Bike-Aplicativo.jpg?alt=media&token=a5714d99-03c9-4df7-bd9e-8e89c3c83f04"
+          content="https://firebasestorage.googleapis.com/v0/b/vela-c1f68.appspot.com/o/public%2Fvelastore%2Frent%2FAlugue-Vela-2-Bain.jpg?alt=media&token=0f92939b-a51b-4d01-afc1-5acff3b41605"
         />
       </Head>
       <div className="cover">
@@ -268,294 +268,297 @@ const Rental = () => {
       </div>
       <style jsx>{`
         @media only screen and (max-width: 320px) {
-            h2 {
-                font-size: 1.2em;
-            }
+          h2 {
+              font-size: 1.2em;
+          }
         }
         .section {
-            display: flex;
-            flex-direction: column;
-            padding-top: 1em;
+          display: flex;
+          flex-direction: column;
+          padding-top: 1em;
         }
         .section h2 {
-            color: ${offBlack};
+          color: ${offBlack};
         }
         .section img {
-            min-width: 100%;
-            min-height: 50vh;
-            max-height: 50vh;
-            object-fit: cover;
+          min-width: 100%;
+          min-height: 50vh;
+          max-height: 50vh;
+          object-fit: cover;
         }
         .actions :global(button) {
-            margin-top: 1em;
-            margin-bottom: 0;
+          margin-top: 1em;
+          margin-bottom: 0;
         }
         .left {
-            display: flex;
-            flex-direction: column;
-            margin: 1em 0;
+          display: flex;
+          flex-direction: column;
+          margin: 1em 0;
         }
         .left-content {
-            padding: 2em 2em 1em 2em;
-            text-align: center;
+          padding: 2em 2em 1em 2em;
+          text-align: center;
         }
         .right {
-            margin: 1em 0;
-            display: flex;
-            flex-direction: column-reverse;
+          margin: 1em 0;
+          display: flex;
+          flex-direction: column-reverse;
         }
         .right-content {
-            padding: 2em 2em 1em 2em;
-            text-align: center;
+          padding: 2em 2em 1em 2em;
+          text-align: center;
         }
         .questions {
-            padding: 2em 0em 0em 0em;
-            background-color: ${offBlack};    
+          padding: 2em 0em 0em 0em;
+          background-color: ${offBlack};    
         }
         .cta h2, .cta h1, .cta h4 {
-            color: ${offWhite}
+          color: ${offWhite}
         }
         .questions h2 {
-            color: ${offWhite};
-            margin-bottom: 1em;
+          color: ${offWhite};
+          margin-bottom: 1em;
         }
         .cover {
-            position: relative;
+          position: relative;
         }
         .cta {
-            text-align: center;
-            padding: 2em;
-            background-color: ${offBlack};
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
+          text-align: center;
+          padding: 2em;
+          background-color: ${offBlack};
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
         }
-        .cta h4, .cta h2 {
+        .cta h4 {
           width: 16em;
         }
+        .cta h2 {
+          width: 12em;
+        }
         .cta-bottom {
-            text-align: center;
-            background-color: ${offBlack};
-            padding: 2rem 3rem 2rem 3rem;
+          text-align: center;
+          background-color: ${offBlack};
+          padding: 2rem 3rem 2rem 3rem;
         }
         .cta-bottom h2 {
-            color: ${offWhite};
+          color: ${offWhite};
         }
         .howitworks {
-            padding: 2em 2em 3em 2em;
+          padding: 2em 2em 3em 2em;
         }
         .howitworks h2{
-            color: ${offBlack};
+          color: ${offBlack};
         }
         .steps {
-            display: flex;
-            flex-direction: column;
+          display: flex;
+          flex-direction: column;
         }
         .step {
-            padding: 2em 1em;
+          padding: 2em 1em;
         }
         .step h4 {
-            font-size: 1.2em;
+          font-size: 1.2em;
         }
         .step img {
-            max-width: 12em;
-            margin-bottom: 1em;
+          max-width: 12em;
+          margin-bottom: 1em;
         }
         .tagline {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            flex-direction: column;
-            text-align: center;
-            padding: 2em 4em 1em 4em;
-            background-color:  ${offWhite};
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+          text-align: center;
+          padding: 2em 4em 1em 4em;
+          background-color:  ${offWhite};
         }
         .tagline h3 {
-            color: ${offBlack};
-            font-style: italic;
+          color: ${offBlack};
+          font-style: italic;
         }
         .tagline h4 {
-            color: ${offBlack};
+          color: ${offBlack};
         }
         .legal {
-            background-color: ${offWhite};
-            padding: 2em 0 2em 0;
+          background-color: ${offWhite};
+          padding: 2em 0 2em 0;
           }
-          .legal-container {
+        .legal-container {
+          display: flex;
+          flex-direction: column;
+        }
+        .legal h3 {
+          color: ${offBlack};
+        }
+        .docs h3 {
+          color: ${offBlack};
+          margin-bottom: 1em;
+          margin-top: 1em;
+        }
+        .protection {
+          z-index: 3;
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          width: 100%;
+          background-color: ${offBlack};
+        }
+        .protection .cta-protect {
+          width: 20em;
+          text-align: center;
+          padding: 2em 2em;
+        }
+        .protection .cta-protect h3, .protection .cta-protect h4 {
+          color: ${offWhite};
+        }
+        .protection .cta-protect p {
+          color: ${offWhite};
+          font-weight: 600;
+        }
+        @media only screen and (min-width: 768px) {
+          .cta {
+            position: absolute;
+            bottom: 0;
             display: flex;
+            justify-content: center;
             flex-direction: column;
+            width: 100%;
+            text-align: center;
+            padding: 2em;
+            background-color: transparent;
           }
-          .legal h3 {
+          .cta h2, .cta h1, .cta h4 {
+            color: ${offWhite}
+          }
+          .cta h4, .cta h2 {
+            width: 100%;
+          }
+          .protection .cta-protect h3, .protection .cta-protect h4 {
             color: ${offBlack};
           }
-          .docs h3 {
+          .protection .cta-protect p {
             color: ${offBlack};
-            margin-bottom: 1em;
-            margin-top: 1em;
+            font-weight: 600;
           }
           .protection {
-            z-index: 3;
-            position: relative;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
+            align-items: flex-end;
+            justify-content: flex-start;
             width: 100%;
             background-color: ${offBlack};
           }
           .protection .cta-protect {
-            width: 20em;
-            text-align: center;
-            padding: 2em 2em;
+            width: 22em;
+            text-align: left;
+            padding: 3em;
+            position: absolute;
+            z-index: 4;
+            right: 8em;
+            top: 4em;
+            background-color: ${white}
           }
-          .protection .cta-protect h3, .protection .cta-protect h4 {
+          .questions {
+            padding: 3em 4em 0em 4em;
+          }
+          .howitworks {
+            padding: 3em 3em 2em 3em;
+          }
+          .howitworks h2 {
+            padding-left: 1em;
+          }
+          .steps {
+            display: flex;
+            flex-direction: row;
+          }
+          .step {
+            padding: 2em 3em;
+            max-width: 33%;
+          }
+          .section {
+            padding: 3em 2em 1em 2em;
+            flex-direction: column;
+            align-items: stretch;
+          }
+          .left {
+            flex-direction: row-reverse;
+            margin-bottom: 2em;
+          }
+          .section img {
+            max-width: 32em;
+            min-width: 28em;
+            max-height: 25em;
+            min-height: 25em;
+            object-fit: cover;
+          }
+          .section p, .section h4 {
+            width: 16em;
+          }
+          .right {
+            flex-direction: row-reverse;
+            align-self: start;
+          }
+          .left-content {
+            padding: 3em 4em 2em 2em;
+            text-align: right;
+            display: flex;
+            align-items: flex-end;
+            justify-content: center;
+            flex-direction: column;
+          }
+          .right-content {
+            padding: 3em 2em 2em 4em;
+            text-align: left;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+          }
+          .cta-bottom {
+            position: absolute;
+            justify-content: center;
+            left: 2em;
+            bottom: 2em;
+            text-align: left;
+            background-color: transparent;
+            margin-top: 0rem;
+            padding: 2em;
+            width: 30vw;
+          }
+          .tagline {
+            justify-content: flex-start;
+            align-items: flex-start;
+            top: 2em;
+            right: 4em;
+            text-align: left;
+            position: absolute;
+            max-width: 24em;
+            background-color: transparent;
+          }
+          .tagline h3{
+            color: ${offWhite};
+            font-style: italic;
+          }
+          .tagline h4{
             color: ${offWhite};
           }
-          .protection .cta-protect p {
-            color: ${offWhite};
-            font-weight: 600;
+          .image-cut {
+            position: relative;
           }
-        @media only screen and (min-width: 768px) {
-            .cta {
-                position: absolute;
-                bottom: 0;
-                display: flex;
-                justify-content: center;
-                flex-direction: column;
-                width: 100%;
-                text-align: center;
-                padding: 2em;
-                background-color: transparent;
-            }
-            .cta h2, .cta h1, .cta h4 {
-                color: ${offWhite}
-            }
-            .cta h4 {
-              width: 100%;
-            }
-            .protection .cta-protect h3, .protection .cta-protect h4 {
-              color: ${offBlack};
-            }
-            .protection .cta-protect p {
-              color: ${offBlack};
-              font-weight: 600;
-            }
-            .protection {
-                align-items: flex-end;
-                justify-content: flex-start;
-                width: 100%;
-                background-color: ${offBlack};
-            }
-            .protection .cta-protect {
-                width: 22em;
-                text-align: left;
-                padding: 3em;
-                position: absolute;
-                z-index: 4;
-                right: 8em;
-                top: 4em;
-                background-color: ${white}
-            }
-            .questions {
-                padding: 3em 4em 0em 4em;
-            }
-            .howitworks {
-                padding: 3em 3em 2em 3em;
-            }
-            .howitworks h2 {
-                padding-left: 1em;
-            }
-            .steps {
-                display: flex;
-                flex-direction: row;
-            }
-            .step {
-                padding: 2em 3em;
-                max-width: 33%;
-            }
-            .section {
-                padding: 3em 2em 1em 2em;
-                flex-direction: column;
-                align-items: stretch;
-            }
-                        .left {
-                            flex-direction: row-reverse;
-                            margin-bottom: 2em;
-                        }
-                        .section img {
-                            max-width: 32em;
-                            min-width: 28em;
-                            max-height: 25em;
-                            min-height: 25em;
-                            object-fit: cover;
-                        }
-                        .section p, .section h4 {
-                            width: 16em;
-                        }
-                        .right {
-                            flex-direction: row-reverse;
-                            align-self: start;
-                        }
-                        .left-content {
-                            padding: 3em 4em 2em 2em;
-                            text-align: right;
-                            display: flex;
-                            align-items: flex-end;
-                            justify-content: center;
-                            flex-direction: column;
-                        }
-                        .right-content {
-                            padding: 3em 2em 2em 4em;
-                            text-align: left;
-                            display: flex;
-                            justify-content: center;
-                            flex-direction: column;
-                        }
-                        .cta-bottom {
-                            position: absolute;
-                            justify-content: center;
-                            left: 2em;
-                            bottom: 2em;
-                            text-align: left;
-                            background-color: transparent;
-                            margin-top: 0rem;
-                            padding: 2em;
-                            width: 30vw;
-                        }
-                        .tagline {
-                            justify-content: flex-start;
-                            align-items: flex-start;
-                            top: 2em;
-                            right: 4em;
-                            text-align: left;
-                            position: absolute;
-                            max-width: 24em;
-                            background-color: transparent;
-                        }
-                        .tagline h3{
-                            color: ${offWhite};
-                            font-style: italic;
-                        }
-                        .tagline h4{
-                            color: ${offWhite};
-                        }
-                        .image-cut {
-                            position: relative;
-                        }
-                        .legal {
-                            padding: 3em 3em 1em 3em;
-                        }
-                        .docs h3 {
-                            margin-top: 0;
-                        }
-                        .legal-container {
-                            flex-direction: row;
-                            justify-content: space-between;
-                        }
-                        .legal .infos {
-                            max-width: 45%;
-                        }
+          .legal {
+            padding: 3em 3em 1em 3em;
+          }
+          .docs h3 {
+            margin-top: 0;
+          }
+          .legal-container {
+            flex-direction: row;
+            justify-content: space-between;
+          }
+          .legal .infos {
+            max-width: 45%;
+          }
         }
       `}</style>
     </div>
