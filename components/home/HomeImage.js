c2dd2819931fa72c5beaa54ba003283010041fae
gidagel <gidagel@gmail.com>
2021-08-06 19:38:44 +0200

    Last fixes

--- components/home/HomeImage.js
+++ components/home/HomeImage.js
@@ -7,7 +7,7 @@ const HomeImage = () =>
         min-height: 50vh;
         object-fit: cover;
         object-position: right;
-        width: 100vw;
+        width: 100%;
       }
       .desktop {
         display: none;
@@ -16,6 +16,7 @@ const HomeImage = () =>
         img {
           margin-top: 0em;
           object-fit: cover;
+          width: 98.9vw;
           height: calc(100vh - 3.5em);
           object-position: bottom-right;
         }
