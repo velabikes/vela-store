d5c56108cf38f2cf23a87e111f07f2d49ff50c1c
Mastereloading <guilherme-lamon@hotmail.com>
2021-11-25 11:11:14 -0300

    Included services page on header and footer

--- components/Header.js
+++ components/Header.js
@@ -35,6 +35,9 @@ const Header = ({ isCartOpen, isMenuOpen, children, handleContentClick }) =>
         <div className='left'><Link href='/store/parts' as='/loja/pecas'><a>
           Peças
         </a></Link></div>
+        <div className='left'><Link href='/store/services' as='/loja/servicos'><a>
+          Serviços
+        </a></Link></div>
         <div className='left'><Link href='/network/map' as='/rede/mapa'><a>
           Rede
         </a></Link></div>
