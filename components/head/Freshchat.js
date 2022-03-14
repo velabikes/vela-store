8c3e5d5b723db8e03d7369fd79354f4e76fd5a68
Diogo Vianna <diogo@velabikes.com.br>
2022-03-14 09:45:41 -0300

    Update Freshchat.js

--- components/head/Freshchat.js
+++ components/head/Freshchat.js
@@ -3,18 +3,6 @@ import Head from 'next/head'
 import { compose, lifecycle } from 'recompose'
 
 const Freshchat = () =>
-  <Head>
-    <script dangerouslySetInnerHTML={{
-      __html: `
-      (function (o, c, t, a, d, e, s, k) {
-        o.octadesk = o.octadesk || {};
-        s = c.getElementsByTagName(“body”)[0];
-        k = c.createElement(“script”);
-        k.async = 1;
-        k.src = t + ‘/’ + a + ‘?showButton=’ +  d + ‘&openOnMessage=’ + e;
-        s.appendChild(k);
-      })(window, document, ‘https://chat.octadesk.services/api/widget’, ‘velabikes1’,  true, true);
-    `}} />
-  </Head>
+  <script src="https://chat.octadesk.services/api/widget/velabikes1?showButton=true&openOnMessage=true" />
 
 export default Freshchat
