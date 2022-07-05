32d68450e670da460a02b914aa2d0c6703334e02
vhccruz <58791807+vhccruz@users.noreply.github.com>
2022-07-05 16:43:19 -0300

    Update index.js
    360 shipping cost

--- api/shipping/shopify/index.js
+++ api/shipping/shopify/index.js
@@ -111,7 +111,7 @@ module.exports = async (req, res) => {
       )
     }
     else {
-      const totalShippingPrice = 24500 * Math.round(totalGrams / 26000) // Shipping price Vela
+      const totalShippingPrice = 36000 * Math.round(totalGrams / 26000) // Shipping price Vela
       return (
         res.end(JSON.stringify({
           rates: [{
