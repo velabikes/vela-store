9a62fe0ced99fb327bd6419231bbedc389fb056c
vhccruz <58791807+vhccruz@users.noreply.github.com>
2022-11-07 19:14:20 -0300

    Update ParkingButton.js

--- components/locations/ParkingButton.js
+++ components/locations/ParkingButton.js
@@ -1,14 +1,14 @@
 import { NextIcon } from 'components/Icons'
 import { velaGreen } from 'style/colors'
 
-const ParkingButton = () =>
-  <div className='parkingButton'>
-    <a href='http://www.bikeandpark.com.br'>Bike & Park</a>
+const GrudiButton = () =>
+  <div className='grudiButton'>
+    <a href='http://www.velabikes.com.br'>Grudi</a>
     <style jsx>{`
-      .parkingButton{
+      .grudiButton{
           text-align: left;
           margin-top: 1em;
       }
     `}</style>
   </div>
-export default ParkingButton
+export default GrudiButton
