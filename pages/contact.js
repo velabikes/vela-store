dbaf4be1fdddf1bf061d0ccfdf7cda9df773d577
Victor Cruz <victor@velabikes.com.br>
2022-11-10 10:32:49 -0300

    remove whatsapp hours

--- pages/contact.js
+++ pages/contact.js
@@ -46,7 +46,7 @@ const Contact = () => (
         <h3>WhatsApp:</h3>
         <h4>
           <a href="https://api.whatsapp.com/send?phone=5511963934737">((11) 9.6393-4737</a>
-          <br />Fuciona dentro do horário comercial e é a forma mais ágil de comunicação. <b>72 horas</b>.
+          <br />Fuciona dentro do horário comercial e é a forma mais ágil de comunicação.
         </h4>
         <br />
         <h3>Chat:</h3>
