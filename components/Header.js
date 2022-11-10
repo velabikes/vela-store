0dc5ad147e16768a88ee1ebe73c1e6f9049baada
Victor Cruz <victor@velabikes.com.br>
2022-11-10 17:37:58 -0300

    new window bf landing

--- components/Header.js
+++ components/Header.js
@@ -73,7 +73,11 @@ const Header = ({ isCartOpen, isMenuOpen, children, handleContentClick }) => (
           </Link>
         </div>
         <div className="left blackfriday">
-          <Link href="https://bit.ly/BFVela22" as="https://bit.ly/BFVela22">
+          <Link
+            href="https://bit.ly/BFVela22"
+            as="https://bit.ly/BFVela22"
+            target="_blank"
+          >
             <a>| Black Friday |</a>
           </Link>
         </div>
