1e72b0899e225124c4e0ceada94acf2d81796a2d
Diogo Vianna V. Araujo <diogo@velabikes.com>
2022-11-11 17:32:22 -0300

    Removed unavailable filter from collection page.

--- pages/store.js
+++ pages/store.js
@@ -1,50 +1,52 @@
-import PropTypes from 'prop-types'
-import Head from 'next/head'
-import { withRouter } from 'next/router'
-import { compose } from 'recompose'
-import Main from 'components/Main'
-import MainHeader from 'components/MainHeader'
-import ProductList from '../components/store/ProductList'
-import PaddedView from '../components/PaddedView'
-import withCollectionByHandle from '../containers/withCollectionByHandle'
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { withRouter } from "next/router";
+import { compose } from "recompose";
+import Main from "components/Main";
+import MainHeader from "components/MainHeader";
+import ProductList from "../components/store/ProductList";
+import PaddedView from "../components/PaddedView";
+import withCollectionByHandle from "../containers/withCollectionByHandle";
 
-const Store = ({
-  collection,
-  isCollectionLoading
-}) =>
+const Store = ({ collection, isCollectionLoading }) => (
   <PaddedView>
     <Main>
       <Head>
         <title>{collection && collection.title} - Vela Bikes</title>
       </Head>
       <div className="title">
-      <MainHeader
-        title={collection && collection.title}
-      />
-      <h2>{collection && collection.descriptionHtml}</h2>
+        <MainHeader title={collection && collection.title} />
+        <h2>{collection && collection.descriptionHtml}</h2>
       </div>
       <br />
-      <ProductList products={collection && collection.products} loading={isCollectionLoading} />
+      <ProductList
+        products={collection && collection.products}
+        loading={isCollectionLoading}
+      />
     </Main>
-    <style jsx>{`
-      .title {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-      }
-      .title p {
-        margin-top: 0.8em;
-      }
-    `}
-    </style> 
+    <style jsx>
+      {`
+        .title {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+        }
+        .title p {
+          margin-top: 0.8em;
+        }
+      `}
+    </style>
   </PaddedView>
+);
 
 export default compose(
   withRouter,
-  withCollectionByHandle(({ router }) => router.query.handle || 'acessorios', { filterUnavailable: true })
-)(Store)
+  withCollectionByHandle(({ router }) => router.query.handle || "acessorios", {
+    filterUnavailable: false,
+  })
+)(Store);
 
 Store.propTypes = {
   collection: PropTypes.object,
-  isCollectionLoading: PropTypes.bool
-}
+  isCollectionLoading: PropTypes.bool,
+};
