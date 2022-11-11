0e9abce6502b53cbee144e4c8321729ea553a4be
Diogo Vianna V. Araujo <diogo@velabikes.com>
2022-11-11 18:44:54 -0300

    sold image on product list.

--- components/store/ProductTeaser.js
+++ components/store/ProductTeaser.js
@@ -1,30 +1,47 @@
-import PropTypes from 'prop-types'
-import Link from 'next/link'
-import ProductPrice from 'components/store/ProductPrice'
-import ProductImage from './ProductImage'
+import PropTypes from "prop-types";
+import Link from "next/link";
+import ProductPrice from "components/store/ProductPrice";
+import ProductImage from "./ProductImage";
 
-const ProductTeaser = ({ product }) =>
-  <div className='ProductTeaser'>
-    <Link href={'/store/product?handle=' + product.handle} as={'/loja/produto/' + product.handle}><a>
-      <ProductImage src={product.images && product.images.edges[0].node.src} />
-      <h3>
-        {product.title}
-      </h3>
-      <ProductPrice
-        product={product}
-        variant={product.variants.edges.length === 1 && product.variants.edges[0]}
-      />
-    </a></Link>
+const ProductTeaser = ({ product }) => (
+  <div className="ProductTeaser">
+    <Link
+      href={"/store/product?handle=" + product.handle}
+      as={"/loja/produto/" + product.handle}
+    >
+      <a>
+        <ProductImage
+          src={product.images && product.images.edges[0].node.src}
+        />
+        {!product.variants.edges[0].node.availableForSale && (
+          <img src="/product/images/vendida.png" className="sold" />
+        )}
+        <h3>{product.title}</h3>
+        <ProductPrice
+          product={product}
+          variant={
+            product.variants.edges.length === 1 && product.variants.edges[0]
+          }
+        />
+      </a>
+    </Link>
     <style jsx>{`
       h3 {
-        margin: .5rem 0 0;
+        margin: 0.5rem 0 0;
         font-size: 1.25rem;
+      }
+      .sold {
+        margin-top: -100%;
+        z-index: 1;
+        position: relative;
+        top: -15px;
       }
     `}</style>
   </div>
+);
 
 ProductTeaser.propTypes = {
-  product: PropTypes.object
-}
+  product: PropTypes.object,
+};
 
-export default ProductTeaser
+export default ProductTeaser;
