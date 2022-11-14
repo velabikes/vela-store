472244547e908f6f82d8246d9fba3e83756d4798
Diogo Vianna V. Araujo <diogo@velabikes.com>
2022-11-14 20:18:42 -0300

    lower case filter

--- pages/store.js
+++ pages/store.js
@@ -32,19 +32,19 @@ const Store = ({ collection, isCollectionLoading }) => {
         <div className="filter-container">
           <div
             onClick={() => onSelectedFilter("Outlet")}
-            className={filter === "outlet" && "selected"}
+            className={filter === "Outlet" && "selected"}
           >
             <h3>outlet</h3>
           </div>
           <div
             onClick={() => onSelectedFilter("Seminova")}
-            className={filter === "seminova" && "selected"}
+            className={filter === "Seminova" && "selected"}
           >
             <h3>semi-nova</h3>
           </div>
           <div
             onClick={() => onSelectedFilter("Nova")}
-            className={filter === "nova" && "selected"}
+            className={filter === "Nova" && "selected"}
           >
             <h3>nova</h3>
           </div>
