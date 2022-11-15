cecc1484bb46eea32a6899c23b2db0598e06c7e0
Victor Cruz <victor@velabikes.com.br>
2022-11-15 09:43:55 -0300

    black friday header drawer

--- components/MainMenu.js
+++ components/MainMenu.js
@@ -1,25 +1,35 @@
-import Link from 'next/link'
-import { velaRed, offBlack } from '../style/colors'
+import Link from "next/link";
+import { velaRed, offBlack } from "../style/colors";
 
-const MainMenu = () =>
-  <nav className='MainMenu'>
+const MainMenu = () => (
+  <nav className="MainMenu">
     <h2>Menu</h2>
-    <ul className='items'>
-      <li><Link href='/vela'><a>
-        Vela 2
-      </a></Link></li>
-      <li><Link href='/velamais'><a>
-        Vela+
-      </a></Link></li>
-      <li><Link href='/store' as='/loja'><a>
-        Acessórios
-      </a></Link></li>
-      <li><Link href='/store/parts' as='/loja/pecas'><a>
-        Peças
-      </a></Link></li>
-      <li><Link href='/store/services' as='/loja/servicos'><a>
-        Serviços
-      </a></Link></li>
+    <ul className="items">
+      <li>
+        <Link href="/vela">
+          <a>Vela 2</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/velamais">
+          <a>Vela+</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/store" as="/loja">
+          <a>Acessórios</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/store/parts" as="/loja/pecas">
+          <a>Peças</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/store/services" as="/loja/servicos">
+          <a>Serviços</a>
+        </Link>
+      </li>
       {/* <li><Link href='/network/stores' as='/rede/lojas'><a>
         Nossas lojas
       </a></Link></li> */}
@@ -29,28 +39,36 @@ const MainMenu = () =>
       </a></Link></li>
       */}
       <hr />
-      <li><Link href='/network/map' as='/rede/mapa'><a>
-        Rede
-      </a></Link></li>
+      <li>
+        <Link href="/network/map" as="/rede/mapa">
+          <a>Rede</a>
+        </Link>
+      </li>
       {/* <li><Link href='/testride' as='/test-ride'><a>
         Test-ride
       </a></Link></li> */}
-      <li><Link href='/blog'><a>
-        Blog
-      </a></Link></li>
-      <li><Link href='/contact' as='/contato'><a>
-        Contato
-      </a></Link></li>
-      <li><a href='https://app.kria.vc/agents/users/offers/210' target='_blank'>
-        Investir
-      </a></li>
+      <li>
+        <Link href="/blog">
+          <a>Blog</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/contact" as="/contato">
+          <a>Contato</a>
+        </Link>
+      </li>
+      <li>
+        <a href="https://bit.ly/BFVela22" target="_blank">
+          | Black Friday |
+        </a>
+      </li>
     </ul>
     <style jsx>{`
       h2 {
         color: ${offBlack};
       }
       nav {
-      padding: 0.5rem 1rem;
+        padding: 0.5rem 1rem;
       }
       ul {
         padding: 0;
@@ -62,9 +80,10 @@ const MainMenu = () =>
         font-size: 1em;
       }
       li .outlet {
-        color: ${velaRed}
+        color: ${velaRed};
       }
     `}</style>
   </nav>
+);
 
-export default MainMenu
+export default MainMenu;
