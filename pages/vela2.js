764a10bf7a2a3ab0226af37298a8e69c8a171d8c
Diogo Vianna V. Araujo <diogoviannaaraujo@gmail.com>
2023-04-20 16:21:53 -0300

    fix: broken vela2 build

--- pages/vela2.js
+++ pages/vela2.js
@@ -1,142 +1,152 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from 'react'
-import Head from 'next/head'
-import { compose } from 'recompose'
-import Display from '../components/vela2/Display'
-import Bar from '../components/vela2/Bar'
-import AddedToCart from '../components/vela2/AddedToCart'
-import ModelSelector from '../components/vela2/ModelSelector'
-import ExtraSelector from '../components/vela2/ExtraSelector'
-import VelaPlusSelector from '../components/vela2/VelaPlusSelector'
-import Tab from '../components/vela2/Tab'
-import withCheckoutLineItemsAdd from '../containers/withCheckoutLineItemsAdd'
-import withCheckout from '../containers/withCheckout'
-import ModelData from '../components/vela2/ModelData'
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import { compose } from "recompose";
+import Display from "../components/vela2/Display";
+import Bar from "../components/vela2/Bar";
+import AddedToCart from "../components/vela2/AddedToCart";
+import ModelSelector from "../components/vela2/ModelSelector";
+import ExtraSelector from "../components/vela2/ExtraSelector";
+import VelaPlusSelector from "../components/vela2/VelaPlusSelector";
+import Tab from "../components/vela2/Tab";
+import withCheckoutLineItemsAdd from "../containers/withCheckoutLineItemsAdd";
+import withCheckout from "../containers/withCheckout";
+import ModelData from "../components/vela2/ModelData";
 
 const initialValues = {
-	color: 'Verde',
-	size: 'P',
-	tire: 'Creme',
+  color: "Verde",
+  size: "P",
+  tire: "Creme",
 };
-
 
 const Vela2 = ({ checkout, checkoutLineItemsAdd, handleCheckoutCreation }) => {
   const [selectedModel, setSelectedModel] = useState(initialValues);
-  const [selectedExtra, setSelectedExtra] = useState([])
+  const [selectedExtra, setSelectedExtra] = useState([]);
   const [step, setStep] = useState(1);
   const [loadedCheckout, setLoadedCheckout] = useState(false);
 
-  const { size, color } = selectedModel
-  const selectedModelData =
-    ModelData[JSON.stringify({ size, color })] || {}
+  const selectedModelData = ModelData[JSON.stringify(selectedModel)] || {};
 
+  useEffect(() => {
+    if (loadedCheckout) return;
+    if (!checkout) return;
+    const bikeIds = findBikeIdOnCheckout();
+    if (bikeIds.length > 0) {
+      const bike = getModelById(bikeIds[bikeIds.length - 1]);
+      setSelectedModel(bike);
+    }
+    setLoadedCheckout(true);
+  }, [checkout]);
 
-    useEffect(() => {
-      if (loadedCheckout) return;
-      if (!checkout) return;
-      const bikeIds = findBikeIdOnCheckout();
-      if (bikeIds.length > 0) {
-        const bike = getModelById(bikeIds[bikeIds.length - 1]);
-        setSelectedModel(bike);
+  const findBikeIdOnCheckout = () => {
+    const modelIds = getAllModelIds();
+    const bikeIds = [];
+    for (let modelId of modelIds) {
+      const item = findItemOnCheckout(modelId);
+      if (item) {
+        bikeIds.push(modelId);
       }
-      setLoadedCheckout(true);
-    }, [checkout]);
+    }
 
-  
-    const findBikeIdOnCheckout = () => {
-      const modelIds = getAllModelIds();
-      const bikeIds = [];
-      for (let modelId of modelIds) {
-        const item = findItemOnCheckout(modelId);
-        if (item) {
-          bikeIds.push(modelId);
-        }
+    return bikeIds;
+  };
+
+  const getAllModelIds = () => {
+    const models = [];
+    Object.keys(ModelData).forEach((model) => {
+      models.push(ModelData[model].id);
+    });
+
+    return models;
+  };
+
+  const getModelById = (id) => {
+    let bike;
+    Object.keys(ModelData).forEach((model) => {
+      if (ModelData[model].id === id) {
+        bike = model;
       }
-  
-      return bikeIds;
-    };
-  
-  
-    const getAllModelIds = () => {
-      const models = [];
-      Object.keys(ModelData).forEach((model) => {
-        models.push(ModelData[model].id);
-      });
-  
-      return models;
-    };
-  
-    const getModelById = (id) => {
-      let bike;
-      Object.keys(ModelData).forEach((model) => {
-        if (ModelData[model].id === id) {
-          bike = model;
-        }
-      });
-  
-      return JSON.parse(bike);
-    };
-  
-    const findItemOnCheckout = (id) => {
-      return checkout.lineItems.edges.find(
-        (item) => item.node.variant.id === id
-      );
-    };
-  
+    });
+
+    return JSON.parse(bike);
+  };
+
+  const findItemOnCheckout = (id) => {
+    return checkout.lineItems.edges.find((item) => item.node.variant.id === id);
+  };
 
   const handleNext = async () => {
     if (step === 1) {
-      setStep(2)
+      setStep(2);
     }
     if (step === 2) {
-      setStep(3)
+      setStep(3);
     }
 
     if (step === 3) {
-      const checkoutId = await handleCheckoutCreation()
+      const checkoutId = await handleCheckoutCreation();
       await checkoutLineItemsAdd({
         variables: {
           checkoutId,
           lineItems: [
             ...selectedExtra.map(({ id }) => ({
               variantId: id,
-              quantity: 1
+              quantity: 1,
             })),
             {
               variantId: selectedModelData.id,
-              quantity: 1
-            }
-          ]
-        }
-      })
-      setStep(4)
+              quantity: 1,
+            },
+          ],
+        },
+      });
+      setStep(4);
     }
 
     if (step === 4) {
-      {checkout.lineItems.edges.length ? window.location.replace(checkout.webUrl) : setStep(1)}
+      {
+        checkout.lineItems.edges.length
+          ? window.location.replace(checkout.webUrl)
+          : setStep(1);
+      }
     }
-  }
+  };
 
   return (
-    <div className='vela2'>
+    <div className="vela2">
       <Head>
         <title>Monte a sua - Vela Bikes</title>
-        <meta http-equiv='content-language' content='pt-br' />
-        <meta name='application-name' content='Vela Bikes Store' />
-        <meta name='description' content='Escolha o modelo da sua Vela 2, os acessórios essenciais para a cidade e aproveite para pedalar smart!' />
-        <meta property='og:url' content='https://velabikes.com.br/vela2' />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content='Monte a sua - Vela Bikes' />
-        <meta property='og:locale' content='pt_BR' />
-        <meta property='og:description' content='Escolha o modelo da sua Vela 2, os acessórios essenciais para a cidade e aproveite para pedalar smart!' />
-        <meta property='og:image' content='https://firebasestorage.googleapis.com/v0/b/vela-c1f68.appspot.com/o/public%2Fvelastore%2FScreen%20Shot%202022-06-22%20at%2009.15.06.png?alt=media&token=35763e05-5cc3-47f8-ac71-5b0d75d8f9d6' />
+        <meta http-equiv="content-language" content="pt-br" />
+        <meta name="application-name" content="Vela Bikes Store" />
+        <meta
+          name="description"
+          content="Escolha o modelo da sua Vela 2, os acessórios essenciais para a cidade e aproveite para pedalar smart!"
+        />
+        <meta property="og:url" content="https://velabikes.com.br/vela2" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Monte a sua - Vela Bikes" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta
+          property="og:description"
+          content="Escolha o modelo da sua Vela 2, os acessórios essenciais para a cidade e aproveite para pedalar smart!"
+        />
+        <meta
+          property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/vela-c1f68.appspot.com/o/public%2Fvelastore%2FScreen%20Shot%202022-06-22%20at%2009.15.06.png?alt=media&token=35763e05-5cc3-47f8-ac71-5b0d75d8f9d6"
+        />
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@velabikes" /> 
+        <meta name="twitter:site" content="@velabikes" />
         <meta name="twitter:title" content="Monte a sua - Vela Bikes" />
-        <meta name="twitter:description" content="Escolha o modelo da sua Vela 2, os acessórios essenciais para a cidade e aproveite para pedalar smart!'" />
-        <meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/vela-c1f68.appspot.com/o/public%2Fvelastore%2FScreen%20Shot%202022-06-22%20at%2009.15.06.png?alt=media&token=35763e05-5cc3-47f8-ac71-5b0d75d8f9d6" />
+        <meta
+          name="twitter:description"
+          content="Escolha o modelo da sua Vela 2, os acessórios essenciais para a cidade e aproveite para pedalar smart!'"
+        />
+        <meta
+          name="twitter:image"
+          content="https://firebasestorage.googleapis.com/v0/b/vela-c1f68.appspot.com/o/public%2Fvelastore%2FScreen%20Shot%202022-06-22%20at%2009.15.06.png?alt=media&token=35763e05-5cc3-47f8-ac71-5b0d75d8f9d6"
+        />
       </Head>
-      <div className='content'>
+      <div className="content">
         <Display model={selectedModel} />
         <Tab step={step} onStep={setStep}>
           <ModelSelector
@@ -146,10 +156,10 @@ const Vela2 = ({ checkout, checkoutLineItemsAdd, handleCheckoutCreation }) => {
           <ExtraSelector
             model={selectedModel}
             selected={selectedExtra}
-            onSelect={id =>
+            onSelect={(id) =>
               setSelectedExtra(
                 selectedExtra.includes(id)
-                  ? selectedExtra.filter(item => item !== id)
+                  ? selectedExtra.filter((item) => item !== id)
                   : [...selectedExtra, id]
               )
             }
@@ -157,12 +167,13 @@ const Vela2 = ({ checkout, checkoutLineItemsAdd, handleCheckoutCreation }) => {
           <VelaPlusSelector
             model={selectedModel}
             selected={selectedExtra}
-            onSelect={id =>
+            onSelect={(id) =>
               setSelectedExtra(
                 selectedExtra.includes(id)
-                  ? selectedExtra.filter(item => item !== id)
+                  ? selectedExtra.filter((item) => item !== id)
                   : [...selectedExtra, id]
-              )}
+              )
+            }
           />
           <AddedToCart onStep={setStep} />
         </Tab>
@@ -186,10 +197,7 @@ const Vela2 = ({ checkout, checkoutLineItemsAdd, handleCheckoutCreation }) => {
         }
       `}</style>
     </div>
-  )
-}
+  );
+};
 
-export default compose(
-  withCheckout,
-  withCheckoutLineItemsAdd
-)(Vela2)
+export default compose(withCheckout, withCheckoutLineItemsAdd)(Vela2);
