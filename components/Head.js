a8357056010a7cc8cbef91e570a053f15f70a2b1
Victor Cruz <victor@velabikes.com.br>
2023-04-25 13:56:00 -0300

    gtm

--- components/Head.js
+++ components/Head.js
@@ -24,14 +24,15 @@ const CustomHead = () => {
     <div className="Head">
       <Head>
         <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-P7VWQJT');`,
-          }}
-        />
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-F3W6Q7GX9D"
+        ></script>
+        <script>
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}; gtag('js', new Date()); gtag('config',
+          'G-F3W6Q7GX9D');
+        </script>
+
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
--- pages/legal.js
+++ pages/legal.js
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import PaddedView from '../components/PaddedView'
-import Main from '../components/Main'
-import Head from '../components/Head'
-import Tab from '../components/legal/Tab'
-import TermsOfService from '../components/legal/TermsOfService'
-import TermsOfVelaPlus from '../components/legal/TermsOfVelaPlus'
-import PrivacyPolicy from '../components/legal/PrivacyPolicy'
-import ShippingPolicy from '../components/legal/ShippingPolicy'
-import Returns from '../components/legal/Returns'
-import Warranty from '../components/legal/Warranty'
+import { useState } from "react";
+import PaddedView from "../components/PaddedView";
+import Main from "../components/Main";
+import Head from "../components/Head";
+import Tab from "../components/legal/Tab";
+import TermsOfService from "../components/legal/TermsOfService";
+import TermsOfVelaPlus from "../components/legal/TermsOfVelaPlus";
+import PrivacyPolicy from "../components/legal/PrivacyPolicy";
+import ShippingPolicy from "../components/legal/ShippingPolicy";
+import Returns from "../components/legal/Returns";
+import Warranty from "../components/legal/Warranty";
 
 const LegalPage = () => {
   const [step, setStep] = useState(1);
@@ -17,7 +17,7 @@ const LegalPage = () => {
       <Head>
         <title>Informações Legais - Vela Bikes</title>
       </Head>
-      <Main className='legal'>
+      <Main className="legal">
         <PaddedView>
           <h1>Informações Legais</h1>
           <Tab step={step} onStep={setStep}>
@@ -31,7 +31,7 @@ const LegalPage = () => {
         </PaddedView>
       </Main>
     </>
-  )
-}
+  );
+};
 
-export default LegalPage
+export default LegalPage;
