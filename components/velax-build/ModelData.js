c38465c1dcd8bd2b4f65c2c82f03b599128bbabd
Victor Cruz <victor@velabikes.com.br>
2023-04-28 09:16:53 -0300

    UPDATE

--- components/velax-build/ModelData.js
+++ components/velax-build/ModelData.js
@@ -1,13 +1,13 @@
 export default {
-  '{"size":"M","color":"Preto",': {
+  '{"size":"M"}': {
     id: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTcyNjk0NTIwNjMyNw==",
     photos: ["/static/vela2/models/VEL-V2-510-26-PRETO-PRETO.png"],
   },
-  '{"size":"G","color":"Preto"}': {
+  '{"size":"G"}': {
     id: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTcyNjk0NTUzNDAwNw==",
     photos: ["/static/vela2/models/VEL-V2-550-29-PRETO-PRETO.png"],
   },
-  '{"size":"GG","color":"Preto"}': {
+  '{"size":"GG"}': {
     id: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTcyNjk0NTg2MTY4Nw==",
     photos: ["/static/vela2/models/VEL-V2-590-29-PRETO-PRETO.png"],
   },
