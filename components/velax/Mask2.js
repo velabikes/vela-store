ec60f488782072934baa4f70aa052e1177ff8910
vhccruz <victor@velabikes.com.br>
2023-05-23 15:56:44 -0300

    svg group 143 (2)

--- components/velax/Mask2.js
+++ components/velax/Mask2.js
@@ -4,7 +4,7 @@ const Mask = ({ maskSize }) => {
   return (
     <svg
       width="100vw"
-      height="auto"
+      height="100vh"
       viewBox="0 0 3773 2069"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
