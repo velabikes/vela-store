2aba7b28a28a2ab5d7d270bbdad45aee0a157a5f
vhccruz <victor@velabikes.com.br>
2023-06-19 17:03:03 -0300

    lighter webps

--- components/velax/ScrollImageContainer.js
+++ components/velax/ScrollImageContainer.js
@@ -12,7 +12,7 @@ const ScrollImageContainer = () => {
   });
 
   const [currentImage, setCurrentImage] = useState(0);
-  const imageCount = 258; // Replace this with the number of images in your sequence
+  const imageCount = 256; // Replace this with the number of images in your sequence
 
   useEffect(() => {
     const handleScroll = () => {
