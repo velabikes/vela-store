6953317c064169ff1279dedc62958cdbf27e5095
vhccruz <victor@velabikes.com.br>
2023-06-27 13:59:34 -0300

    pra ver

--- components/velax/Lights.js
+++ components/velax/Lights.js
@@ -38,7 +38,7 @@ const Lights = () => {
 
       <div className="lights-text-container">
         <p className="lights-text-tittle">Muita Luz</p>
-        <p className="lights-text">Ver e ser visto.</p>
+        <p className="lights-text">Pra ver e ser visto.</p>
       </div>
 
       <style jsx>{`
