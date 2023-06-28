580d63e9ecda44cacf9c4e1a924ad7087cce2104
vhccruz <victor@velabikes.com.br>
2023-06-28 11:23:43 -0300

    final scroll mobile

--- components/velax/ScrollImageContainerMob.js
+++ components/velax/ScrollImageContainerMob.js
@@ -38,7 +38,7 @@ const ScrollImageContainerMob = () => {
         scrollTrigger: {
           trigger: scrollContainerElement,
           start: "top 0",
-          end: "600%  0 ",
+          end: "1000%  0 ",
           pin: true,
           scrub: 0.1,
           pinSpacing: true,
